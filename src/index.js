0b559bde6506ad069d94bbe4a050e982fffdc10d
Zishan Ansari <zshan.ansari07@gmail.com>
2020-07-31 16:02:11 +0530

    Added API Calls: Redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import 'babel-polyfill';
 
-const root = document.getElementById('root');
+import Root from './containers/Root';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  root
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Root />, document.getElementById('root'));
